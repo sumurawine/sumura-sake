790864839da40cff1ac363077d42a153b29f5276
sumurawine <rechargeable@icloud.com>
2026-07-31 21:57:27 +0900

    fix: ホームに更新履歴・メルマガ案内・本日の一本のパネルを追加

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -23,6 +23,34 @@ export function HomePage() {
         <T k="home-p2" as="p" />
         <T k="home-p3" as="p" />
       </div>
+
+      <div className="panel">
+        <T k="home-hist-head" as="div" kind="head" className="pixhead" />
+        <ul className="dots">
+          <T k="home-hist-1" as="li" />
+          <T k="home-hist-2" as="li" />
+          <T k="home-hist-3" as="li" />
+        </ul>
+      </div>
+
+      <div className="panel">
+        <T k="home-newsletter-head" as="div" kind="head" className="pixhead" />
+        <T k="home-newsletter-body" as="p" />
+      </div>
+
+      <div className="panel" style={{ textAlign: 'center' }}>
+        <img src={asset('/images/bottle.png')} alt="" />
+        {'　'}
+        <T k="home-today" as="span" />
+        {'　'}
+        <img src={asset('/images/bottle.png')} alt="" />
+        <br />
+        <T k="home-today-desc" as="span" className="hint" />
+        <br />
+        <a href="store.html">
+          <T k="home-store-btn" as="span" kind="btn" className="btn" style={{ display: 'inline-block', marginTop: 10 }} />
+        </a>
+      </div>
     </Shell>
   );
 }
