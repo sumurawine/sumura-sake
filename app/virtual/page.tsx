a334f868e2ceed92d0330435aea9d3d38998b5a5
sumurawine <rechargeable@icloud.com>
2026-08-03 02:40:32 +0900

    年齢のご確認・バーチャル店舗・時間旅行

---
+++ app/virtual/page.tsx
@@ -0,0 +1,4 @@
+import { metaPrivate } from '@/lib/siteMeta';
+import { VirtualPage } from '@/components/pages/VirtualPage';
+export default function Page() { return <VirtualPage />; }
+export const metadata = metaPrivate('バーチャル店舗');
