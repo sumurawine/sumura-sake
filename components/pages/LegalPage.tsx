1e00ae8739e4593aca8977144deab2a042eb9783
sumurawine <rechargeable@icloud.com>
2026-08-05 18:02:37 +0900

    特商法の代表者名を翻訳に載せる

--- components/pages/LegalPage.tsx
+++ components/pages/LegalPage.tsx
@@ -10,8 +10,8 @@ export function LegalPage() {
         <table className="retro" style={{ fontSize: 13, lineHeight: 1.7 }}>
           <tbody>
             <tr><T k="law-seller-label" as="th" style={{ width: '30%' }} /><T k="law-seller-val" as="td" /></tr>
-            <tr><T k="law-rep-label" as="th" /><td>洲村博志</td></tr>
-            <tr><T k="law-ops-label" as="th" /><td>洲村博志</td></tr>
+            <tr><T k="law-rep-label" as="th" /><T k="law-rep-val" as="td" /></tr>
+            <tr><T k="law-ops-label" as="th" /><T k="law-rep-val" as="td" /></tr>
             <tr><T k="law-address-label" as="th" /><T k="law-address-val" as="td" /></tr>
             <tr><T k="law-phone-label" as="th" /><td>0836-21-4721</td></tr>
             <tr><T k="law-email-label" as="th" /><td>hiroshi@sumura-sake.com</td></tr>
