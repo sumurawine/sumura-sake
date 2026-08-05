f58239de74a1148c58ebf6df9a99166a6a76b29d
sumurawine <rechargeable@icloud.com>
2026-08-05 17:11:09 +0900

    点で組まれた書体（DotGothic16）を読み込む

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="stylesheet" href={`${BP}/editor.css?v=${CSSV}`} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;1,300&family=Shippori+Mincho:wght@400;600&family=Zen+Kaku+Gothic+New:wght@400;500&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;1,300&family=DotGothic16&family=Shippori+Mincho:wght@400;600&family=Zen+Kaku+Gothic+New:wght@400;500&display=swap" />
         <script dangerouslySetInnerHTML={{ __html: eraInit }} />
         <script src={`${BP}/ov.js?v=${CSSV}`} />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(shopLd) }} />
