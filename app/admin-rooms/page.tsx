527588b4b829c36ba12bd14e7150da001c90a6d7
sumurawine <rechargeable@icloud.com>
2026-08-06 15:30:59 +0900

    鍵のかかった部屋の入口

---
+++ app/admin-rooms/page.tsx
@@ -0,0 +1,4 @@
+import { metaPrivate } from '@/lib/siteMeta';
+import { AdminRoomsPage } from '@/components/pages/AdminRoomsPage';
+export default function Page() { return <AdminRoomsPage />; }
+export const metadata = metaPrivate('鍵のかかった部屋');
