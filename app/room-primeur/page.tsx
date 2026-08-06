38f56a937e79eae6eeb485438d333663a42195dc
sumurawine <rechargeable@icloud.com>
2026-08-06 15:29:28 +0900

    中身が漏れていた部屋の頁を消す

--- app/room-primeur/page.tsx
+++
@@ -1,4 +0,0 @@
-import { metaPrivate } from '@/lib/siteMeta';
-import { RoomPrimeurPage } from '@/components/pages/RoomPrimeurPage';
-export default function Page() { return <RoomPrimeurPage />; }
-export const metadata = metaPrivate('プリムール');
